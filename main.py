ac3911431a571da371b77b1f6252a76d8e204bda
chelovek7114 <chelovek71148@gmail.com>
2015-05-05 01:09:34 +0600

    + вход

---
+++ main.py
@@ -0,0 +1,72 @@
+__author__ = 'CHELOVEK7114'
+
+
+class SocialNetwork:
+    def __init__(self):
+        pass
+
+    def hello1(self, answer):
+        return 'Здравствуйте, вы зарегистрированы? (да, нет) ', None
+
+    def hello2(self, answer):
+        if answer == 'да':
+            return 'Вход. Введите логин: ', None
+        elif answer == 'нет':
+            return 'Регистрация. Введите логин: ', None
+        else:
+            return 'Вы зарегистрированы? (да, нет) ', None
+
+    def sign_in(self, answer):
+        if answer:
+            return 'Вход. Введите пароль: ', None
+        else:
+            return 'Вход. Логин не может быть пустым. Повторите ввод: ', None
+
+    def sign_in2(self, answer, login):
+        if login == 'Вася':
+            return 'Здравствуйте, {0}. Что вы хотите? '.format(login), None
+        else:
+            return 'Неправильный логин или пароль. Нажмите Enter для продолжения. ', None
+
+# self.cur_person = None
+
+#     def sign_in(self):
+#         answer = input('Вы зарегистрированы? (да/нет/выход): ')
+#         while not (answer in ('да', 'нет', 'выход')):
+#             answer = input('Вы зарегистрированы? Введите "да", "нет" или "выход": ')
+#         if answer == 'выход':
+#             return
+#         if answer == 'да':
+#             name = input('Введите ваш логин: ')
+#             password = input('Введите ваш пароль: ')
+#             self.cur_person = None
+#             for person in self.persons:
+#                 if name == person.name and password == person.password:
+#                     self.cur_person = person
+#                     break
+#             if self.cur_person:
+#                 self.menu()
+#             else:
+#                 print('Неправильно введён логин или пароль')
+#                 self.sign_in()
+#         else:
+#             name = input('Введите логин: ')
+#             while name == '':
+#                 name = input('Логин не может быть пустым. Повторите ввод: ')
+#             password = input('Введите пароль: ')
+#             while password == '':
+#                 password = input('Логин не может быть пустым. Повторите ввод: ')
+#             self.cur_person = Person(name, password)
+#             self.persons.append(self.cur_person)
+#             self.menu()
+#
+#     def menu(self):
+#         print('Hello ' + self.cur_person.name + '!!!')
+#
+#
+# class Person:
+#     def __init__(self, name, password):
+#         self.password = password
+
+
+#         self.name = name
---
+++ test.py
@@ -0,0 +1,42 @@
+from unittest import TestCase
+from main import SocialNetwork
+
+__author__ = 'CHELOVEK7114'
+
+
+class TestSocialNetwork(TestCase):
+    def test_hello(self):
+        sn = SocialNetwork()
+        fact = sn.hello1('')[0]
+        expected = 'Здравствуйте, вы зарегистрированы? (да, нет) '
+        self.assertEqual(fact, expected)
+
+    def test_hello2(self):
+        sn = SocialNetwork()
+        fact = sn.hello2('да')[0]
+        expected = 'Вход. Введите логин: '
+        self.assertEqual(fact, expected)
+        fact = sn.hello2('нет')[0]
+        expected = 'Регистрация. Введите логин: '
+        self.assertEqual(fact, expected)
+        fact = sn.hello2('не знаю')[0]
+        expected = 'Вы зарегистрированы? (да, нет) '
+        self.assertEqual(fact, expected)
+
+    def test_sign_in(self):
+        sn = SocialNetwork()
+        fact = sn.sign_in('Вася')[0]
+        expected = 'Вход. Введите пароль: '
+        self.assertEqual(fact, expected)
+        fact = sn.sign_in('')[0]
+        expected = 'Вход. Логин не может быть пустым. Повторите ввод: '
+        self.assertEqual(fact, expected)
+
+    def test_sign_in2(self):
+        sn = SocialNetwork()
+        fact = sn.sign_in2('пароль', 'Вася')[0]
+        expected = 'Здравствуйте, Вася. Что вы хотите? '
+        self.assertEqual(fact, expected)
+        fact = sn.sign_in2('пароль', 'Петя')[0]
+        expected = 'Неправильный логин или пароль. Нажмите Enter для продолжения. '
+        self.assertEqual(fact, expected)
